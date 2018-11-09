7f21504619f2fb37543c773cf30a065b4955b4b0
antoine.fr@epita.coulon <antoine@coulon>
2018-11-10 00:00:12 +0100

    [map] update .c & .h

--- src/map.c
+++ src/map.c
@@ -2,5 +2,41 @@
 
 struct map *create_map(char *path)
 {
-    
+    FILE *file = fopen(path, "r");
+    struct map *my_map = malloc(sizeof(struct map));
+    my_map->table = malloc(sizeof(struct block **));
+    while (fread(buffer, 3, sizeof(char), file) > 0)
+    {
+
+
+    }
+}
+
+void free_map(struct map *my_map)
+{
+    for (int i = 0; i < map->height; i++)
+    {
+        for (int j = 0; j < map->width; j++)
+            free(map->table[i][j]);
+        free(map->table[i]);
+    }
+    free(map->table);
+    free(map);
+}
+
+void print_map (struct map *my_map)
+{
+    for (int i = 0; i < map->height; i++)
+    {
+        for (int j = 0; j < map->width; j++)
+            printf("%d", map->table[i][j]);
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    struct map *m = create_map("maps/map1");
+    print_map();
+    return 0;
 }
