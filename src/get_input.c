2457414c466484cee02838e5a907697b7e20d90c
Alexandre Girard <alexandre.girard@epita.fr>
2018-11-10 15:24:18 +0100

    [input] linking get_input with project

--- src/get_input.c
+++ src/get_input.c
@@ -12,85 +12,39 @@ void get_input(struct GameContext *GC)
     {
         switch (key)
         {
-            case SDLK_UP:
-                GC->player1->event = FORWARD;
-                return;
-            case SDLK_DOWN:
-                GC->player1->event = BACK;
-                return;
-            case SDLK_LEFT:
-                GC->player1->event = LEFT;
-                return;
-            case SDLK_RIGHT:
-                GC->player1->event = RIGHT;
-                return;
-            case SDLK_KP_0:
-                GC->player1->is_shoting = 1;
-                return;
-            case SDLK_w:
-                GC->player2->event = FORWARD;
-                return;
-            case SDLK_s:
-                GC->player2->event = BACK;
-                return;
-            case SDLK_a:
-                GC->player2->event = LEFT;
-                return;
-            case SDLK_d:
-                GC->player2->event = RIGHT;
-                return;
-            case SDLK_SPACE:
-                GC->player2->is_shoting = 1;
-                return;
-            case SDLK_ESCAPE:
-                GC->isPlaying = 0;
-                return;
-            default:
-                return;
+            case SDLK_UP: GC->player1->event = FORWARD; return;
+            case SDLK_DOWN: GC->player1->event = BACK; return;
+            case SDLK_LEFT: GC->player1->event = LEFT; return;
+            case SDLK_RIGHT: GC->player1->event = RIGHT; return;
+            case SDLK_KP_0: GC->player1->is_shoting = 1; return;
+            case SDLK_w: GC->player2->event = FORWARD; return;
+            case SDLK_s: GC->player2->event = BACK; return;
+            case SDLK_a: GC->player2->event = LEFT; return;
+            case SDLK_d: GC->player2->event = RIGHT; return;
+            case SDLK_SPACE: GC->player2->is_shoting = 1; return;
+            case SDLK_ESCAPE: GC->isPlaying = 0;return;
+            default: return;
         }
     }
     else if (event.type == SDL_KEYUP)
     {
         switch (key)
         {
-            case SDLK_UP:
-                GC->player1->event = NOTHING;
-                return;
-            case SDLK_DOWN:
-                GC->player1->event = NOTHING;
-                return;
-            case SDLK_LEFT:
-                GC->player1->event = NOTHING;
-                return;
-            case SDLK_RIGHT:
-                GC->player1->event = NOTHING;
-                return;
-            case SDLK_KP_0:
-                GC->player1->is_shoting = 0;
-                return;
-            case SDLK_w:
-                GC->player2->event = NOTHING;
-                return;
-            case SDLK_s:
-                GC->player2->event = NOTHING;
-                return;
-            case SDLK_a:
-                GC->player2->event = NOTHING;
-                return;
-            case SDLK_d:
-                GC->player2->event = NOTHING;
-                return;
-            case SDLK_SPACE:
-                GC->player2->is_shoting = 0;
-                return;
-            case SDLK_ESCAPE:
-                GC->isPlaying = 0;
-                return;
-            default:
-                return;
+            case SDLK_UP: GC->player1->event = NOTHING; return;
+            case SDLK_DOWN: GC->player1->event = NOTHING; return;
+            case SDLK_LEFT: GC->player1->event = NOTHING; return;
+            case SDLK_RIGHT: GC->player1->event = NOTHING; return;
+            case SDLK_KP_0: GC->player1->is_shoting = 0; return;
+            case SDLK_w: GC->player2->event = NOTHING; return;
+            case SDLK_s: GC->player2->event = NOTHING; return;
+            case SDLK_a: GC->player2->event = NOTHING; return;
+            case SDLK_d: GC->player2->event = NOTHING; return;
+            case SDLK_SPACE: GC->player2->is_shoting = 0; return;
+            case SDLK_ESCAPE: GC->isPlaying = 0;return;
+            default: return;
         }
 
     }
     else
-        GC->isPlaying = 0;
+        return;
 }
--- src/main.c
+++ src/main.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
         errx(1, "Please enter path for map like: ./toto <path>");
-    SDL_Event event;
     gameInit(&game, argv[1]);
     SDL_Window *window = window_init(game.map);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
@@ -24,30 +23,10 @@ int main(int argc, char *argv[])
     game.player1->position->x = 1;
     game.player1->position->y = 1;
 
-    int quit = 0;
-    while (!quit)
+    while (game.isPlaying)
     {
-        SDL_WaitEvent(&event);
-        switch (event.type)
-        {
-            case SDL_QUIT:
-                quit = 1;
-                break;
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym)
-                {
-                    case SDLK_LEFT:  game.player2->position->x -= 0.1; break;
-                    case SDLK_RIGHT: game.player2->position->x += 0.1; break;
-                    case SDLK_UP:    game.player2->position->y -= 0.1; break;
-                    case SDLK_DOWN:  game.player2->position->y += 0.1; break;
-                    case SDLK_q:  game.player1->position->x -= 0.1; break;
-                    case SDLK_d: game.player1->position->x += 0.1; break;
-                    case SDLK_z:    game.player1->position->y -= 0.1; break;
-                    case SDLK_s:  game.player1->position->y += 0.1; break;
-                    case SDLK_p:  quit = 1; break;
-                }
-                break;
-        }
+        get_input(&game);
+        update(&game);
         render_map(game.map, renderer);
         render_tanks(&game, renderer);
         SDL_RenderPresent(renderer);
