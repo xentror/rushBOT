4cff7647a8c66f710de1dfd40cdcfd4a6fa329ad
Antoine Coulon <antoine.coulon@epita.fr>
2018-11-10 16:30:55 +0100

    [update] remove bullets from AI behaviour

--- src/update.c
+++ src/update.c
@@ -12,13 +12,6 @@ static void update_AI_behaviour(struct GameContext *GC)
             shot(GC, GC->enemies[i]);
         }
     }
-
-    for (int i = 0; i < GC->nb_bullets; i++)
-    {
-        struct bullet *B = GC->bullets[i];
-        B->position->x += B->direction->x * B->speed;
-        B->position->y += B->direction->y * B->speed;
-    }
 }
 
 void update(struct GameContext *GC)
